7d749bc47a2de90f271c4d700cf84a7b31bc7cea
Kacper Serewiś <kacper@kacperserewis.net>
2022-08-27 13:51:42 +0200

    add util to retry actions

---
+++ src/helpers/retryify.ts
@@ -0,0 +1,53 @@
+import { fetchLandingPageCms } from "../redux/reducers/landing-page.reducer";
+import { fetchSocials } from "../redux/reducers/socials.reducer";
+import { store } from "../redux/store";
+import { delay } from "./sleep";
+
+class Retryify {
+
+    private currentTry = 0;
+    constructor(private action: () => Promise<any>, private verifyAction: () => Promise<boolean>, private maxTries: any, private tryEveryMs: number) { }
+
+    async start() {
+        return new Promise(async (resolve, reject) => {
+
+            while (this.currentTry < this.maxTries) {
+                let result = await this.retry();
+                if (result) return resolve(1);
+                else await delay(this.tryEveryMs)
+
+                this.currentTry += 1;
+
+            }
+
+            return reject("Maximum tries reached!")
+
+        })
+
+
+
+    }
+
+    private async retry(): Promise<boolean> {
+        await this.action();
+        return await this.verifyAction();
+
+    }
+}
+
+
+export function SOCIALS_FETCH_RETRYIFY(tries = 5, pauseBetween = 1000) {
+    return new Retryify(async () => {
+        await store.dispatch(fetchSocials());
+    }, async () => {
+        return !store.getState().socialsReducer.error
+    }, tries, pauseBetween)
+}
+
+export function LANDING_PAGE_CMS_RETRYIFY(tries = 5, pauseBetween = 1000) {
+    return new Retryify(async () => {
+        await store.dispatch(fetchLandingPageCms());
+    }, async () => {
+        return !store.getState().landingPageReducer.error
+    }, tries, pauseBetween)
+}
