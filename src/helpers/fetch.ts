363e54de132dc52d8623213681006a3cf3807a30
Kacper Serewiś <kacper@kacperserewis.net>
2022-08-27 13:51:48 +0200

    add mocking of fetch

--- src/helpers/fetch.ts
+++ src/helpers/fetch.ts
@@ -1,4 +1,10 @@
+import { IApiResponseModel } from "../models/api-response.model";
+import { ISocialModel } from "../models/social.model";
+
 export async function fetchWrapper<T>(url: string): Promise<T> {
+    if (!navigator.onLine) {
+        return await offlineMock<T>(url) as T;
+    }
 
     let response = await fetch(url);
 
@@ -7,5 +13,21 @@ export async function fetchWrapper<T>(url: string): Promise<T> {
     }
 
     return await response.json() as T;
+}
+
+const SOCIALS_MOCK: IApiResponseModel<ISocialModel[]> = {
+    data: [{ id: "", name: "Mock 1" + new Date().getSeconds(), url: "" },]
+}
+
+async function offlineMock<T>(url: string): Promise<T> {
+    console.log("Test", url)
+
+    switch (url) {
+        case "https://99sxsqgh.directus.app/items/Socials":
+            return SOCIALS_MOCK as any;
+        case "https://99sxsqgh.directus.app/items/LandingPage":
+        default:
+            throw new Error("Failed to mock " + url)
+    }
 
 }
