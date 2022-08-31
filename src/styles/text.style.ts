7d276a289f0007737e863b767b8eec4200c7e175
Kacper Serewiś <kacper@kacperserewis.net>
2022-08-31 11:27:39 +0200

    fix clamps

--- src/styles/text.style.ts
+++ src/styles/text.style.ts
@@ -2,16 +2,16 @@ import { css } from 'lit';
 
 export const textStyle = css`
     .main-text {
-        font-size: clamp(3em, 15vw, 5em); 
+        font-size: clamp(3em, 10vw, 5em); 
         font-weight: bold;
     }
 
     .sub-text {
-        font-size: clamp(2.5em, 15vw, 1.5em);
+        font-size: clamp(1.5em, 10vw, 2.5em);
     }
 
     .bot-text {
-        font-size: clamp(1.5em, 15vw, 1em);
+        font-size: clamp(1em, 10vw, 1.5em);
     }
 
 
