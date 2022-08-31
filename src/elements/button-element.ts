212a9b34fdfbc411271ef9d996dedc0871abc08c
Kacper Serewiś <kacper@kacperserewis.net>
2022-08-31 11:28:09 +0200

    use another button, add logic

--- src/elements/button-element.ts
+++ src/elements/button-element.ts
@@ -9,20 +9,72 @@ export class ButtonElement extends LitElement {
     @property({ type: String })
     path?: string;
 
+    @property({type: Boolean})
+    externalPath = false;
+
     static override styles = css`
 
-    .button {
-        border-radius: 5px;
-        background: var(--secondary-color);
-        border: 0;
-        color: var(--background-color)  ;
-        padding: 10px;
-            /* 388087, 6fb3b8, badfe7, c2edce, f6f6f2 */
-    }
 
-    .button:hover {
-        background: var(--primary-color);
-    }
+
+/* From uiverse.io by @alexmaracinaru */
+.cta {
+ position: relative;
+ margin: auto;
+ padding: 12px 18px;
+ transition: all 0.2s ease;
+ border: none;
+ background: none;
+}
+
+.cta:before {
+ content: "";
+ position: absolute;
+ top: 0;
+ left: 0;
+ display: block;
+ border-radius: 50px;
+ background: var(--primary-color-light);
+ width: 45px;
+ height: 45px;
+ transition: all 0.3s ease;
+}
+
+.cta span {
+ position: relative;
+ font-family: "Ubuntu", sans-serif;
+ font-size: 18px;
+ font-weight: 700;
+ letter-spacing: 0.05em;
+ color: var(--primary-color);
+}
+
+.cta svg {
+ position: relative;
+ top: 0;
+ margin-left: 10px;
+ fill: none;
+ stroke-linecap: round;
+ stroke-linejoin: round;
+ stroke: var(--primary-color);
+ stroke-width: 2;
+ transform: translateX(-5px);
+ transition: all 0.3s ease;
+}
+
+
+
+.cta:hover:before {
+ width: 100%;
+}
+
+
+.cta:hover svg {
+ transform: translateX(0);
+}
+
+.cta:active {
+ transform: scale(0.95);
+}
     
     `;
 
@@ -30,9 +82,17 @@ export class ButtonElement extends LitElement {
     testProperty = '';
     override render() {
         return html`
-            <button class="button" @click="${() => Router.go(this.path ?? '/')}" class="element">
+            <!-- <button class="button" @click="${() => Router.go(this.path ?? '/')}" class="element">
                 <slot></slot>
-            </button>
+            </button> -->
+
+            <button class="cta" @click="${() => this.externalPath ? location.href = this.path ?? '' : Router.go(this.path ?? '/')}">
+  <span><slot></slot></span>
+  <svg viewBox="0 0 13 10" height="10px" width="15px">
+    <path d="M1,5 L11,5"></path>
+    <polyline points="8 1 12 5 8 9"></polyline>
+  </svg>
+</button>
         `;
     }
 }
