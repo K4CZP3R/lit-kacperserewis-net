e341d9fe24e527b2fdc96e53349471a54e2ba3d3
Kacper Serewiś <kacper@kacperserewis.net>
2022-08-31 11:27:51 +0200

    make more responsive

--- src/pages/landing-page.ts
+++ src/pages/landing-page.ts
@@ -36,6 +36,10 @@ export class LandingPage extends connect(store)(LitElement) {
         /* animation: appearSlideUp 1s ease; */
     }
 
+    .content {
+        display: flex; flex-direction: column;
+    }
+
     blob3d-element {
         animation: appear 2s ease;
     }
@@ -59,6 +63,8 @@ export class LandingPage extends connect(store)(LitElement) {
     }
 
 
+
+
     
 
 
@@ -69,6 +75,7 @@ export class LandingPage extends connect(store)(LitElement) {
         padding-left: 1%;
         min-height: 40px;
         animation: appear 1s ease;
+        flex-wrap: wrap;
     }
     .socials > a {
         text-decoration: none;
@@ -83,6 +90,23 @@ export class LandingPage extends connect(store)(LitElement) {
             position: absolute;
             z-index: -1;
         }
+
+        .content {
+            align-items: center;
+        }
+
+        .socials {
+            /* display: none */
+            justify-content: space-evenly;
+        }
+
+        .sub-text {
+            display: none
+        }
+
+        .bot-text {
+        text-align: center;
+    }
     }
 
 
@@ -104,6 +128,23 @@ export class LandingPage extends connect(store)(LitElement) {
         SOCIALS_FETCH_RETRYIFY().start().then(() => console.log('fetched socials!')).catch((e) => console.log('socials error', e));
         LANDING_PAGE_CMS_RETRYIFY().start().then(() => console.log('fetched cms!')).catch((e) => console.log('cms error', e));
 
+
+        window.addEventListener('resize', () => {
+            this.blobScreenWidthCheck();
+        });
+
+        this.blobScreenWidthCheck();
+
+    }
+
+    blobScreenWidthCheck() {
+        if(this.changingBlob) return;
+
+        if(window.innerWidth < 768 && !this.simpleBlob) {
+            this.blobToggleTo(true);
+        } else if(window.innerWidth > 768 && this.simpleBlob) {
+            this.blobToggleTo(false);
+        }
     }
 
     override stateChanged(_state: IReduxState): void {
@@ -123,13 +164,22 @@ export class LandingPage extends connect(store)(LitElement) {
 
     }
 
+
+    blobToggleTo(shouldBeSimple: boolean) {
+        this.changingBlob = true;
+
+        setTimeout(() => {
+            this.simpleBlob = shouldBeSimple;
+            this.changingBlob = false;
+        }, 125);
+    }
     blobClick() {
         this.changingBlob = true;
 
         setTimeout(() => {
             this.simpleBlob = !this.simpleBlob;
             this.changingBlob = false;
-        }, 500);
+        }, 125);
 
     }
 
@@ -141,22 +191,19 @@ export class LandingPage extends connect(store)(LitElement) {
         <div class="element">
         
 
-        ${this.landingPage ? html` <div style="display: flex; flex-direction: column;">
+        ${this.landingPage ? html` <div class="content">
 
-<a class="main-text testje">${this.landingPage?.mainText}</a>
-    <a class="sub-text testje">${this.landingPage?.subText}</a>
-    <a class="bot-text testje">${this.landingPage?.botText}</a>
+    <a class="main-text">${this.landingPage?.mainText}</a>
+    <a class="sub-text">${this.landingPage?.subText}</a>
+    <a class="bot-text">${this.landingPage?.botText}</a>
     
     <div class="socials">
-        ${['Projects', 'Blog', 'Projects/ts'].map((link: string) => {
-        return html`<a class="social-link" href="${link}">${link}</a>`;
+        ${['Projects', 'Blog'].map((link: string) => {
+        return html`<button-element class="social-link" .path="${link}">${link}</button-element>`;
     })}
         ${this.socials.map((social) => {
-        return html`<a class="social-link" href="${social.url}">${social.name}</a>`;
+        return html`<button-element class="social-link" ?externalPath="${true}" .path="${social.url}">${social.name}</button-element>`;
     })}
-
-
-
     </div>
 
 
